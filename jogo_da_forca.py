d398a00c8fae619be2e0b29d515defc827ceddae
limaalan <alanlimamarques6@gmail.com>
2022-07-07 20:31:11 -0300

    visual em ascii
    Visualização das vidas restantes em ascii e correção de bugs

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,63 +1,76 @@
 import json
 import random
-
-#from numpy import correlate
+from visual import visual_ascii
 
 #carrega o arquivo json na variável 
 with open ('words.json','r') as f:
     dados = json.load(f)
+f.close
 
 def palavra_aleatoria ():
     #escolhe palavra aleatória para o jogo
     palavra= random.choice(dados['data'])
-    print(palavra)
+    #print(palavra)
     return palavra
 
-def jogo(palavra):
+# Função que revela apenas as letras que o jogador jogou , e informa as vidas restantes
+def imprime_jogo(chutes, palavra,vidas):
+    print("Seus palpites até aqui: ",end='')
+    for j in chutes:
+        print (j,end=' ')
+    print("\n")
+    for i in palavra :
+        if i in chutes:
+            print (i,end=' ')
+        else :
+            print('-',end=' ')
+    print(visual_ascii[vidas])
+    print("Vidas :",vidas)
 
-    vidas = 2
+#função que dada uma palavra e a quantidade de vidas, faz uma rodada do jogo da forca.
+def jogo(palavra,vidas):
+
     corretos=0
     chutes=[] 
     chute = ''
     
     while ( corretos  < (len (palavra)) and vidas >0):
-
-        chute = input("Digite uma letra :")
+        imprime_jogo(chutes,palavra,vidas)
+        chute = input("Digite uma letra :").lower()
         #Caso o jogador já deu esse palpite
         if ( chute in chutes ):
-            print("Você já deu esse palpite!",end="")
+            print("Você já deu esse palpite!")
 
         #Caso contrário verifica se está na palavra
         else :
             chutes.append(chute) 
 
-            if ( chute not in palavra):
-                vidas -=1
-            else :
+            if ( chute in palavra):
                 for i in palavra:
-                    if (i in chutes):
-                        print(i,end=" ")
-                        if ( chute == i ):
-                            corretos+=1
-                    else :
-                        print('-',end=" ")
-            
-            print(" vidas = ",vidas)
-        #print(f"\ncorretos:{corretos}\n")
+                    if (chute ==i ):
+                        corretos +=1
+            else :
+                vidas -=1
+            #print(" vidas = ",vidas)
+        print("---------------------------------")
+
 
     if (vidas):
-        print("Você ganhou !")
+        imprime_jogo(chutes,palavra,vidas)
+        print("Você ganhou ! a palavra era :",palavra)
     else :
-        print("Você perdeu!")      
+        imprime_jogo(chutes,palavra,vidas)
+        print("Você perdeu!, a palavra era :",palavra)  
 
 def main():
+    vidas=6
     opt = ''
     while opt!='sair':
         palavra = palavra_aleatoria()
-        jogo(palavra)
+        jogo(palavra,vidas)
 
         opt = input("Continuar jogando ? digite 'sair' para sair : ")
+        print("---------------------------------------------------")
 
 main()
 
-f.close
